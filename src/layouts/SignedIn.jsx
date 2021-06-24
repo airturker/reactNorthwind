0fef08d9872bddf018050fad73bf26f7cd24ce55
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-06-24 09:34:59 +0300

    23 haziran ders sonu

--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -1,22 +1,18 @@
-import React from "react";
-import { Dropdown, Image, Menu } from "semantic-ui-react";
+import React from 'react'
+import { Dropdown, Menu,Image } from 'semantic-ui-react'
 
 export default function SignedIn({signOut}) {
-  return (
-    <div>
-      <Menu.Item>
-        <Image
-          avatar
-          spaced="right"
-          src="https://i.pinimg.com/280x280_RS/19/50/2a/19502afb5b6d026bb99247eafb64522c.jpg"
-        />
-        <Dropdown pointing="top left" text="turker">
-          <Dropdown.Menu>
-            <Dropdown.Item text="bilgilerim" icon="info" />
-            <Dropdown.Item onClick={signOut} text="çıkış yap" icon="sign-out" />
-          </Dropdown.Menu>
-        </Dropdown>
-      </Menu.Item>
-    </div>
-  );
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://pbs.twimg.com/profile_images/1404479261566713856/_MklDkhx_400x400.jpg"/>
+                <Dropdown pointing="top left" text="Engin">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+        </div>
+    )
 }
